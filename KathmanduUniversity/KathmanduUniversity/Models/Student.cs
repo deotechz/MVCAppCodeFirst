cc2ddb16c2eb691c7c67d912ca9986b4e0cc068b
deoram <deoram@mtradeasia.com>
2019-11-15 15:03:43 +0545

    Initial commit

---
+++ KathmanduUniversity/KathmanduUniversity/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace KathmanduUniversity.Models
+{
+	public class Student
+	{
+		public int ID { get; set; }
+		public string LastName { get; set; }
+		public string FirstMidName { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+
+		public virtual ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
